3057edf7247b91a4d45e2a410d6e402dee2d2e06
bluebird-0 <dlsgkr5581@gmail.com>
2019-09-06 00:20:21 +0900

    기능 : Permission Check 기능 추가 권한 실행 창 타이밍, 페이지 이동 수정 필요 함

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.testapplication
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.provider.Settings
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
+import android.util.Log
 
 import android.view.*
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -24,7 +32,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
+    private val PERMISSIONS_REQUEST_CODE = 1000
     lateinit var mRecognizer: SpeechRecognizer
     lateinit var audioIntent: Intent
 
@@ -35,6 +43,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        checkPermissions()
         //ADDMOB
         MobileAds.initialize(this)
         val adRequest = AdRequest.Builder().build()
@@ -103,6 +112,53 @@ class MainActivity : AppCompatActivity() {
         fab.setOnTouchListener(touchListener)
     }
 
+    private fun checkPermissions() {
+        if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+                val permissions : Array<String> = arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                requestPermissions(permissions,  PERMISSIONS_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        var checked = false
+        if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) ||
+                shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO) ||
+                shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE) )
+        {
+            checked = true
+        }
+        if(requestCode == PERMISSIONS_REQUEST_CODE) {
+            if(grantResults.size > 0){
+                for (grantResult in grantResults){
+                    if (grantResult != PackageManager.PERMISSION_GRANTED)
+                    {
+                        if (checked == false) {//checked [ Don't Ask Again ]
+                            Toast.makeText(applicationContext, "잠시 후에 설정창으로 이동합니다. '권한'에서 권한을 허용해 주세요", Toast.LENGTH_LONG).show()
+                            Handler().postDelayed(Runnable {
+                                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                val uri = Uri.fromParts("package", packageName, null)
+                                intent.data = uri
+                                startActivity(intent)
+                            }, 3000)
+                        }else {
+                            AlertDialog.Builder(this)
+                                    //.setTitle(R.string.app_name)
+                                    .setTitle("알림")
+                                    .setMessage("앱 사용을 위해 권한이 필요합니다")
+                                    .setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+                                        this@MainActivity.finish()
+                                    })
+                                    .show()
+                        }
+                    }
+                }
+            }
+        }
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 
     private fun fab_plus() {
         val intent = Intent(this, WriteActivity::class.java)
@@ -110,7 +166,6 @@ class MainActivity : AppCompatActivity() {
         overridePendingTransition(R.anim.open_activity, R.anim.hold_activity)
     }
     private fun fab_mic() {
-        //음성인식 권한 체크
         mRecognizer.startListening(audioIntent)
     }
     private fun fab_cam() {
